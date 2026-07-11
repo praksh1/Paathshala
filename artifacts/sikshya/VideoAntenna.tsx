652a77bdb79592f6293e168e10a54ef9a3a2787d
praksh1 <praksh.dhakal@gmail.com>
2026-07-11 15:05:30 -0500

    Built the magic video antenna

---
+++ artifacts/sikshya/VideoAntenna.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Daily from '@daily-co/react-native-daily-js';
+
+export default function VideoAntenna() {
+  // This tells the app to turn on the magic walkie-talkie!
+  let callObject = Daily.createCallObject();
+
+  return null;
+}
